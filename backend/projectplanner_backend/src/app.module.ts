99208b399bb83cbed05a7c54b9b65773ef5fe721
TommyTechh <TommyTechh@gmail.com>
2023-01-03 19:15:20 +0100

    Added .env file to use secrets for accessing database in backend project

--- backend/projectplanner_backend/src/app.module.ts
+++ backend/projectplanner_backend/src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { ConfigService } from "@nestjs/config/dist";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tag, Task, User } from "./task/entity/task";
 import { TaskModule } from "./task/task.module";
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'password',
-            database: 'projectplanner',
-            entities: [Task, Tag, User],
-            synchronize: true,
-            logging: true
+        ConfigModule.forRoot({isGlobal: true}),
+        TypeOrmModule.forRootAsync({
+            inject:[ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_NAME'),
+                entities: [Task, Tag, User],
+                synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+                logging: configService.get<boolean>('DB_LOGGING')
+            })
           }),
         TaskModule
     ],
